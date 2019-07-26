ab48a76e2b23c3687294e0b67c621f277e60a11a
WS 12 <jeanyvelleredondo@gmail.com>
2019-07-26 14:46:59 +0800

    onload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function light(sw) {
   var pic;
   if (sw == 0) {
-    pic = "pic_bulbon.gif"
+    pic = "img/pic_bulbon.gif"
   } else {
-    pic = "pic_bulboff.gif"
+    pic = "img/pic_bulboff.gif"
   }
   document.getElementById('myImage').src = pic;
 }
