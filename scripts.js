db6f00b71e5b270c65eb2b7372737ff9aeb8bb33
WS 12 <jeanyvelleredondo@gmail.com>
2019-07-26 14:22:57 +0800

    new branch

---
+++ scripts.js
@@ -0,0 +1,11 @@
+<script>
+function light(sw) {
+  var pic;
+  if (sw == 0) {
+    pic = "pic_bulbon.gif"
+  } else {
+    pic = "pic_bulboff.gif"
+  }
+  document.getElementById('myImage').src = pic;
+}
+</script>
